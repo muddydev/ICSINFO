f0e16493275f931facc6af4b75436763d5f3641d
muddydev <51953831+muddydev@users.noreply.github.com>
2022-06-22 09:32:45 +0100

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,13 @@
+﻿$current = (Get-Date -format "HH:mm")
+$end = (Get-Date "17:30" -format "HH:mm")
+$minutes = (New-TimeSpan -start $current -end $end).TotalMinutes
+Write-Output $minutes
+#param($minutes = 428)
+
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds 60
+  #$myshell.sendkeys(".")
+  $myshell.sendkeys("{NUMLOCK}{NUMLOCK}")
+}
